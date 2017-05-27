b7808ba9e983829030b3d1930d959dfd35aec3d4
Navaneshwar <navane.mula@gmail.com>
2017-05-27 20:24:31 +0530

    changed the tab size

--- sorting.cpp
+++ sorting.cpp
@@ -1,36 +1,29 @@
 //Navaneshwar reddy 106116056
-<<<<<<< HEAD
-//again added another feature to my name 
-//say now gopal 106116098
-//another change:p
-=======
->>>>>>> subbranch1
 #include<iostream>
 using namespace std;
 int main()
 { 
 int n,i,temp,j;
 cin>>n;
-	int ad[n];
-	for(i=0;i<n;i++)
+int ad[n];
+for(i=0;i<n;i++)
+{
+	cin>>ad[i];
+}
+for(i=0;i<n;i++)
+{
+	for(j=i+1;j<n;j++)
 	{
-		cin>>ad[i];
-	}
-	for(i=0;i<n;i++)
-	{
-
-		for(j=i+1;j<n;j++)
+		if(ad[i]>ad[j])
 		{
-			if(ad[i]>ad[j])
-			{
-				temp=ad[i];
-				ad[i]=ad[j];
-				ad[j]=temp;
-				
-			}
+			temp=ad[i];
+			ad[i]=ad[j];
+			ad[j]=temp;
+			
 		}
 	}
-	for(i=0;i<n;i++)
+}
+for(i=0;i<n;i++)
 	{
 		cout<<ad[i]<<endl;
 	}
