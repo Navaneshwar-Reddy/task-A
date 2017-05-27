212f25bfc8dc09cb306e95ae3968918676b07598
Navaneshwar <navane.mula@gmail.com>
2017-05-27 15:56:44 +0530

    added sorting.cpp

---
+++ sorting.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+int n,i,temp,j;
+cin>>n;
+	int ad[n];
+	for(i=0;i<n;i++)
+	{
+		cin>>ad[i];
+	}
+	for(i=0;i<n;i++)
+	{
+
+		for(j=i+1;j<n;j++)
+		{
+			if(ad[i]>ad[j])
+			{
+				temp=ad[i];
+				ad[i]=ad[j];
+				ad[j]=temp;
+				
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		cout<<ad[i]<<endl;
+	}
+	
+}
